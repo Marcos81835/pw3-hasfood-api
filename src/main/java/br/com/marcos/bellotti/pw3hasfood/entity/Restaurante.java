4896452a92e8e12be8b362ca69964b13d671c9ca
Marcos Bellotti <marcos@test.com>
2022-03-28 08:36:20 -0300

    Criacao da Entidade Restaurante

---
+++ src/main/java/br/com/marcos/bellotti/pw3hasfood/entity/Restaurante.java
@@ -0,0 +1,57 @@
+package br.com.marcos.bellotti.pw3hasfood.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Restaurante {
+	
+	@Getter
+	@Setter
+	@Entity
+	@Table(name="TBL_RESTAURANTE")
+	public class Viagem {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		@Column(name="ID_RESTAURANTE")
+		private Long id;
+		
+		@Column(name="TX_CNPJ")
+		private String cnpj;
+		
+		@Column(name="TX_NOME")
+		private String nome;
+		
+		@Column(name="TX_DESCRICAO")
+		private String descricao;
+		
+		@Column(name="DT_APROVACAO")
+		private LocalDate aprovacao;
+		
+		@Column(name="VLR_ENTREGA")
+		private Double entrega;
+		
+		@Column(name="NR_TEMPO_MIN_ENTREGA")
+		private int minimo;
+		
+		@Column(name="NR_TEMPO_MAX_ENTREGA")
+		private int maximo;
+		
+		@ManyToOne
+		@JoinColumn(name="ID_TIPO_COZINHA")
+		private TipoCozinha cozinha;
+		
+	}
+
+}
